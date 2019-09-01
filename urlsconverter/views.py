c6fb3d214ab8d9734d8887a558cdf23337613afa
Andrewmazyar <maziarandriy@gmail.com>
2019-09-01 15:20:07 +0300

    code link

--- urlsconverter/views.py
+++ urlsconverter/views.py
@@ -2,7 +2,7 @@ from django.views import View
 from .models import Urlshorter
 from .forms import UrlForms
 from .shortener import Shorter
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 
@@ -10,22 +10,24 @@ class UrlsViews (View):
     model = Urlshorter
     form_class = UrlForms
     template_name = 'main.html'
+    context_name = 'link'
+
+    def get(request, token):
+        long_url = Urlshorter.objects.filter(url_short=token)
+        return render(request, long_url.url_long())
 
     def post(self, request):
         form = self.form_class(request.POST)
         a = ''
         if form.is_valid():
-            new_url = form.save(commit=False)
+            new_url = form.save()
             a = Shorter().issue()
             new_url.url_short = a
             new_url.save()
-            # return HttpResponseRedirect('/success/')
+            return HttpResponseRedirect('/')
         else:
             form = UrlForms
             a = 'invalid url'
 
         return render(request, self.template_name, {'form': form, 'a': a})
 
-    def get(request, token):
-        long_url = Urlshorter.objects.filter(url_short=token)[0]
-        return redirect(long_url.url_long())
